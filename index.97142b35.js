f674e9b392eff933fca026d61c400eeb47ce3ff2
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-29 16:35:49 +0000

    Deploying to gh-pages from @ VladyslavaGoit/goit-js-hw-10@ce94088ef7ac918bf4a90e144cc9a143ce284dc4 🚀

--- index.97142b35.js
+++
@@ -1,2 +0,0 @@
-!function(){function n(n){var t="/name/".concat(n),e="".concat(" https://restcountries.com/v3.1").concat(t,"?fields=name,capital,population,flags,languages");return fetch(e).then((function(n){if(!n.ok)throw new Error(n.statusText);return n.json()}))}document.querySelector("#search-box").addEventListener("input",(function(t){n(t.currentTarget.value).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}))}))}();
-//# sourceMappingURL=index.97142b35.js.map
