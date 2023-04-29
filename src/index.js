90c42e73a848cdc82104d693b2760982c497883d
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-29 17:03:39 +0300

    add fetchCountries()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+input.addEventListener('input', onInput);
+function onInput(event) {
+  const name = event.currentTarget.value;
+  fetchCountries(name)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
